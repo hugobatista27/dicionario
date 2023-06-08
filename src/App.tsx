6435f35eebc6dcb517d73e9845ffa075ebbf9554
hugobatista27 <hugobatista2710@gmail.com>
2023-06-08 15:51:55 -0300

    refactor: Context API tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const ValueContexts: any = []
   return (
   <MainContext.Provider value={{ValueContexts}}>
-
+    
   </MainContext.Provider>
   )
 }
---
+++ src/contexts/MainContext.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+const MainContext: any = React.createContext<undefined>(undefined);
+
+export default MainContext;
