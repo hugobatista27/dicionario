84c8561e87bbd7099f7681482e5fca4a87da905c
hugobatista27 <hugobatista2710@gmail.com>
2023-06-11 10:05:12 -0300

    feat: cria o componente header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,38 @@
+import styled from 'styled-components';
+
+//imgs
+import LOGO_BOOK from './assets/livro.svg';
+
+// components 
+import { Switcher } from './SwitcherVisionMode';
+
+import MainContext from '../../contexts/MainContext';
+import { useContext } from 'react';
+
+const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    height: 150px;
+
+    select {
+        border: none;
+    }
+`;
+
 export function Header() {
+    const { selectedFont, setSelectedFont } = useContext(MainContext);
+
     return (
-        <header>
-            titulo
-        </header>
+        <StyledHeader>
+            <img src={LOGO_BOOK} alt="logo" />
+            <select name="" id="">
+                <option value="serif">Serif</option>
+                <option value="noserif">San Serif</option>
+                <option value="mono">Mono</option>
+            </select>
+            <Switcher/>
+        </StyledHeader>
     )
 }
