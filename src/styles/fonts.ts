43a077a452907c9856a96420178010f92a719c4f
hugobatista27 <hugobatista2710@gmail.com>
2023-06-13 14:32:41 -0300

    feat: cria um modelo com as fontes

---
+++ src/styles/fonts.ts
@@ -0,0 +1,14 @@
+export const FONTS_OPTIONS = [
+    {
+        fontName: 'Lora',
+        style: 'Serif'
+    },
+    {
+        fontName: 'Open Sans',
+        style: 'Sans-Serif'
+    },
+    {
+        fontName: 'Roboto Mono',
+        style: 'Monospace'
+    },
+]
